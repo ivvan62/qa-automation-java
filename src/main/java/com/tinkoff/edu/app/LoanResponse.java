2a8b3414be266ea07e48474fc776eefb9b752e44
i.i.kuznetsov <ivvan62@yandex.ru>
2021-08-23 14:43:30 +0300

    add Practice quest №6 (version 2)

--- src/main/java/com/tinkoff/edu/app/LoanResponse.java
+++ src/main/java/com/tinkoff/edu/app/LoanResponse.java
@@ -2,14 +2,9 @@ package com.tinkoff.edu.app;
 
 public class LoanResponse {
     private final LoanResponseType responseType;
-    private int responseId;
 
     public LoanResponse(LoanResponseType responseType) {
         this.responseType = responseType;
-    }
-
-    public int save () {
-        return ++responseId;
     }
 
 }
